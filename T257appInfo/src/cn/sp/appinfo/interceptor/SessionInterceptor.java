03bd511870a1b1077d92e6ddc58e75b7b5e675f3
T256appinfo <810055479@qq.com>
2019-11-28 16:56:11 +0800

    拦截器功能已经完成

---
+++ T257appInfo/src/cn/sp/appinfo/interceptor/SessionInterceptor.java
@@ -0,0 +1,19 @@
+package cn.sp.appinfo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SessionInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return super.preHandle(request, response, handler);
+	}
+	
+	
+
+}
